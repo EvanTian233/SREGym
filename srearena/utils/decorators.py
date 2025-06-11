9d8854b48823d14477aa770a9a31a9b348e6d566
Jackson Clark <jclark58@illinois.edu>
2025-06-11 13:43:37 -0500

    Update name

--- srearena/utils/decorators.py
+++ srearena/utils/decorators.py
@@ -2,7 +2,7 @@
 def mark_fault_injected(method):
     def wrapper(self, *args, **kwargs):
         result = method(self, *args, **kwargs)
-        self.is_fault_injected = method.__name__ == "inject_fault"
+        self.fault_injected = method.__name__ == "inject_fault"
         return result
 
     return wrapper
