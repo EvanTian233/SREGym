5dcc612e652ed1c8bdffcfc5a0869824d32de623
Saleha <saleha@d-172-18-122-85.vpn.net.virginia.edu>
2025-06-25 03:14:22 +0500

    fixed kubectl error

--- srearena/conductor/problems/taint_no_toleration.py
+++ srearena/conductor/problems/taint_no_toleration.py
@@ -6,6 +6,7 @@ from srearena.conductor.oracles.pod_scheduled_mitigation import PodScheduledMiti
 from srearena.generators.fault.inject_virtual import VirtualizationFaultInjector
 from srearena.service.apps.socialnet import SocialNetwork        
 from srearena.utils.decorators import mark_fault_injected
+from srearena.service.kubectl import KubeCtl
 
 class TaintNoToleration(Problem):
     def __init__(self):
@@ -13,6 +14,7 @@ class TaintNoToleration(Problem):
         self.namespace = self.app.namespace
         self.faulty_service = "frontend"
         self.faulty_node = "worker1"
+        self.kubectl = KubeCtl()  
 
         super().__init__(app=self.app, namespace=self.namespace)
 
@@ -32,8 +34,18 @@ class TaintNoToleration(Problem):
     @mark_fault_injected
     def inject_fault(self):
         print("Fault Injection")
-        self.injector.inject_toleration_without_matching_taint(
-            [self.faulty_service], node_name=self.faulty_node
+        self.kubectl.exec_command(
+        f"kubectl taint node {self.faulty_node} sre-fault=blocked:NoSchedule --overwrite")
+
+        patch = """[{"op": "add", "path": "/spec/template/spec/tolerations", "value": [
+            {"key": "dummy-key", "operator": "Exists", "effect": "NoSchedule"}
+        ]}]"""
+        self.kubectl.exec_command(
+            f"kubectl patch deployment {self.faulty_service} -n {self.namespace} --type='json' -p='{patch}'"
+        )
+
+        self.kubectl.exec_command(
+            f"kubectl delete pod -l app={self.faulty_service} -n {self.namespace}"
         )
 
     @mark_fault_injected
