8d3996303fdcb0c3b5bbcab140c38b2fb5cdac51
Yiming Su <yiming34@illinois.edu>
2025-08-10 15:30:45 -0500

    getting prompts and max step from config file
    Signed-off-by: Yiming Su <yiming34@illinois.edu>

--- clients/stratus/stratus_utils/get_starting_prompt.py
+++ clients/stratus/stratus_utils/get_starting_prompt.py
@@ -2,14 +2,19 @@ import yaml
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 
 
-def get_init_prompts(self, app_summary):
-    with open(self.prompts_file_path, "r") as file:
-        data = yaml.safe_load(file)
-        sys_prompt = data["system"].format(max_round=self.max_round)
-        user_prompt = data["user"]
+def get_init_prompts(config_path):
+    with open(config_path, "r") as config_file:
+        config = yaml.safe_load(config_file)
+        max_step = config["max_step"]
+        prompt_file = open(config["prompts_path"], "r")
+        prompts = yaml.safe_load(prompt_file)
+        sys_prompt = prompts["system"]
+        user_prompt = prompts["user"].format(max_step=max_step)
         prompts = []
         if sys_prompt:
             prompts.append(SystemMessage(sys_prompt))
         if user_prompt:
             prompts.append(HumanMessage(user_prompt))
+
+        prompt_file.close()
         return prompts
