0fc2858c4d435df60be7cc0d38f042e8dff49424
Yiming Su <yiming34@illinois.edu>
2025-08-14 11:05:10 -0500

    added 1st version of rollback agent, need to test with mitigation agent later
    Signed-off-by: Yiming Su <yiming34@illinois.edu>

--- clients/stratus/stratus_agent/rollback_agent.py
+++ clients/stratus/stratus_agent/rollback_agent.py
@@ -1,10 +1,16 @@
+import asyncio
 import logging
+from pathlib import Path
 
+import yaml
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.constants import END
 from langgraph.graph import START
 
+from clients.stratus.llm_backend.init_backend import get_llm_backend_for_tools
 from clients.stratus.stratus_agent.base_agent import BaseAgent
+from clients.stratus.stratus_utils.get_starting_prompt import get_starting_prompts
+from clients.stratus.stratus_utils.str_to_tool import str_to_tool
 from clients.stratus.tools.stratus_tool_node import StratusToolNode
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
@@ -49,3 +55,56 @@ class RollbackAgent(BaseAgent):
 
         memory = MemorySaver()
         self.graph = self.graph_builder.compile(checkpointer=memory)
+
+
+def main():
+    file_parent_dir = Path(__file__).resolve().parent
+    rollback_agent_config_path = file_parent_dir.parent / "configs" / "rollback_agent_config.yaml"
+    rollback_agent_config = yaml.safe_load(open(rollback_agent_config_path, "r"))
+    max_step = rollback_agent_config["max_step"]
+    prompt_path = file_parent_dir.parent / "configs" / rollback_agent_config["prompts_path"]
+    sync_tools = []
+    async_tools = []
+    tool_descriptions = ""
+    if rollback_agent_config["sync_tools"] is not None:
+        for sync_tool_struct in rollback_agent_config["sync_tools"]:
+            sync_tools.append(str_to_tool(sync_tool_struct))
+            tool_descriptions += sync_tool_struct["description"] + "\n\n"
+    else:
+        sync_tools = None
+    if rollback_agent_config["async_tools"] is not None:
+        for async_tool_struct in rollback_agent_config["async_tools"]:
+            async_tools.append(str_to_tool(async_tool_struct))
+            tool_descriptions += async_tool_struct["description"] + "\n\n"
+    else:
+        async_tools = None
+
+    submit_tool = str_to_tool(
+        {
+            "name": "submit_tool",
+            "description": """
+                The tool to submit benchmark results
+
+                    Args:
+                        ans (str): the answer you would like to submit to the benchmark
+        """,
+        }
+    )
+
+    agent = RollbackAgent(
+        llm=get_llm_backend_for_tools(),
+        max_step=max_step,
+        sync_tools=sync_tools,
+        async_tools=async_tools,
+        submit_tool=submit_tool,
+        tool_descs=tool_descriptions,
+    )
+    agent.build_agent()
+    agent.save_agent_graph_to_png()
+
+    res = asyncio.run(agent.arun(get_starting_prompts(prompt_path, max_step=max_step)))
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
