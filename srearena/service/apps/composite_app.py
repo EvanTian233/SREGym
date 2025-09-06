84e9e0ece7fac58019217e2782c6574288172021
Yiming Su <yiming34@illinois.edu>
2025-09-06 13:26:56 -0500

    added some debug prints

--- srearena/service/apps/composite_app.py
+++ srearena/service/apps/composite_app.py
@@ -9,18 +9,22 @@ from srearena.service.apps.base import Application
 class CompositeApp:
     def __init__(self, apps: list[Application]):
         self.apps = apps
+        self.name = "CompositeApp"
 
     def deploy(self):
         # FIXME: this can be optimized to parallel deploy later
         for app in self.apps:
+            print(f"[CompositeApp] Deploying {app.name}...")
             app.deploy()
 
     def start_workload(self):
         # FIXME: this can be optimized to parallel start later
         for app in self.apps:
+            print(f"[CompositeApp] Starting workload for {app.name}...")
             app.start_workload()
 
     def cleanup(self):
         # FIXME: this can be optimized to parallel cleanup later
         for app in self.apps:
+            print(f"[CompositeApp] Cleaning up {app.name}...")
             app.cleanup()
